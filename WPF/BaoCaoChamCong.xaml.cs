b3d4c96297a3743864b656a2f4df0afe1044da0f
HuyVu163 <vuhuy2048@gmail.com>
2025-03-09 18:32:41 +0700

    Làm xong phần báo cáo quản lý số ngày công

---
+++ WPF/BaoCaoChamCong.xaml.cs
@@ -0,0 +1,51 @@
+﻿using BusinessObjects.Models;
+using DataAssetObjects;
+using Repositories.Repository;
+using Services;
+using Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for BaoCaoChamCong.xaml
+    /// </summary>
+    public partial class BaoCaoChamCong : Window
+    {
+        private readonly ILeaveRequestService _leaveRequestService;
+        public ObservableCollection<LeaveSummary> leaveSummaryList { get; set; } = new ObservableCollection<LeaveSummary>();
+
+        public BaoCaoChamCong()
+        {
+            var _context = new HrmanagementContext();
+            var leaveRequestDAO = new LeaveRequestDAO(_context);
+            var leaveRequestRepo = new LeaveRequestRepository(leaveRequestDAO);
+            _leaveRequestService = new LeaveRequestService(leaveRequestRepo);
+            InitializeComponent();
+        }
+
+        private void GenerateReport_Click(object sender, RoutedEventArgs e)
+        {
+            // Lấy giá trị tháng từ ComboBox
+            int month = int.Parse((MonthComboBox.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "0");
+            int year = int.Parse(YearTextBox.Text);
+
+            var leaveSummaryList = _leaveRequestService.GetLeaveSummary(-1, month, year);
+            ReportDataGrid.ItemsSource = leaveSummaryList;
+        }
+
+    }
+}
